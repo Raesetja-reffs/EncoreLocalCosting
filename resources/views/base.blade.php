bf762ea8611564ab54628f75e68ea1223b79d11b
KyleWestran <kyle@lsystems.co.za>
2024-03-05 09:25:12 +0200

    security policy

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,6 +4,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'EDI Local Costing')</title>
     <link rel="icon" href="{{asset('images/icon-light-small.jpg')}}" type="image/icon type">
